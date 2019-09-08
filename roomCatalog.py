de1827127854ab329644dc888d2355a93aea51ac
Luca Gioacchini <38500957+lucagioacchini@users.noreply.github.com>
2019-09-08 19:25:00 +0200

    Add files via upload

--- roomCatalog.py
+++ roomCatalog.py
@@ -9,9 +9,40 @@ HOST = "0.0.0.0"
 PORT = 9090
 
 class WebService():
+	""" Class implementing a web service which exposes the room catalog,
+	enabling external actors to retrieve information from it and update it.
+
+
+	Methods
+	-------------
+		GET( *uri, **params)
+			an http method GET used to return information from the roomcatalog json file,
+			thanks to parameters passed through URL of get request
+		
+		POST(*uri, **params) 
+			an http method POST used to update the roomcatalog json file,
+			with parameters passed through URL of POST request	
+		
+		DELETE( *uri, **params) 
+			an http method DELETE used to remove from the roomcatalog json file,
+			devices and sensors which are leaving the system
+		
+	"""
 	exposed = True
 	
 	def GET(self, *uri, **params):
+		""" Http GET method that in base of the uri parameters passed during the Client request,
+		returns data from the room catalog json file.
+
+		Paramaters 
+		------------
+			*uri : tuple
+				list of elements in the request format, enclosed in / / after the port specification
+
+			**params :dict
+				dictionary of parameters passed in a 'key-value' format after the ? character in the URL
+
+		"""
 		with open("jsons/room_catalog.json", "r") as jf:
 			rc = json.load(jf) 
 			rc = rc["roomcatalog"]
@@ -25,6 +56,20 @@ class WebService():
 			return json.dumps(rc[3])
 	
 	def POST(self, *uri, **params):
+		"""Http POST method that in base of the uri parameters passed during the Client request,
+		update the roomcatalog json file. the information stored in room catalog json file
+		that can be updated are : thresholds, devices, sensors. This allows scalability and an easy
+		way to adapt threshold parameters to the specific needs 
+		
+		Paramaters 
+		------------
+			*uri : tuple
+				list of elements in the request format, enclosed in / / after the port specification
+
+			**params : dict
+				dictionary of parameters passed in a 'key-value' format after the ? character in the URL
+		
+		"""
 		dev_in_rc = False
 		sensor_list = []
 		if uri[0] == "th":		
@@ -75,6 +120,19 @@ class WebService():
 				jf.write(json.dumps(rc))
 		
 	def DELETE(self, *uri, **params):
+		"""Http DELETE method that in base of the uri parameters passed during the Client request,
+		update the roomcatalog json file. In this case is used to remove from the lists
+		devices and sensors that are disconnected from the system
+		
+		Paramaters 
+		------------
+			*uri : tuple
+				list of elements in the request format, enclosed in / / after the port specification
+
+			**params : dict
+				dictionary of parameters passed in a 'key-value' format after the ? character in the URL
+		
+		"""
 		if uri[0] == "devices":
 			with open("jsons/room_catalog.json", "r") as jf:
 				rc = json.load(jf)
